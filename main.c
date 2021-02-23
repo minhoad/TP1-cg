9ed24e71c370c312b73e16933fff5428c28bc530
minhoad <darmes2000@hotmail.com>
2021-02-23 19:39:05 -0300

    Adicionei pause

--- main.c
+++ main.c
@@ -6,12 +6,18 @@
 #include"global.h"
 #include<stdbool.h>
 #include"texturas.h"
+#include<time.h>
+#include<stdlib.h>
+
+void contador();
+void gameloop(int tempo);
 
 Jogador personagem_principal;
 Personagens inimigo;
-Arma shuriken;
+Arma shuriken; // aliado
+Arma kunai; // inimigo
 formacao matriz_inimigos[3][10];
-
+bool pause = false;
 bool flag = true; // !=0
 
 void desenhaPersonagemPrincipal(){
@@ -93,27 +99,71 @@ void desenhaFundo(){
     glDisable(GL_TEXTURE_2D);
 }
 
-void desenhaProjetil(){
+void desenhaPause(){
+    
     glColor3f(1,1,1);
     
     glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, id_textura_projetil);
-    glPushMatrix();
-
-    glTranslatef(shuriken.posX,shuriken.posY,0);
+    glBindTexture(GL_TEXTURE_2D, id_pause);
     
     glBegin(GL_TRIANGLE_FAN);
         glTexCoord2f(0,0);
-        glVertex2f(-shuriken.largura/2, -shuriken.altura/2);
+        glVertex2f(0, 0);
 
         glTexCoord2f(1,0);
-        glVertex2f(shuriken.largura/2, -shuriken.altura/2);
+        glVertex2f(100,0);
         
         glTexCoord2f(1,1);
-        glVertex2f(shuriken.largura/2, shuriken.altura/2);
+        glVertex2f(100,100);
 
         glTexCoord2f(0,1);
-        glVertex2f(-shuriken.largura/2, shuriken.altura/2);
+        glVertex2f(0,100);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+}
+
+void desenhaProjetil(int x){
+    glColor3f(1,1,1);
+    
+    glEnable(GL_TEXTURE_2D);
+    if(x==1){
+        glBindTexture(GL_TEXTURE_2D, id_textura_projetil);
+        glPushMatrix();
+
+        glTranslatef(shuriken.posX,shuriken.posY,0);
+        
+        glBegin(GL_TRIANGLE_FAN);
+            glTexCoord2f(0,0);
+            glVertex2f(-shuriken.largura/2, -shuriken.altura/2);
+
+            glTexCoord2f(1,0);
+            glVertex2f(shuriken.largura/2, -shuriken.altura/2);
+            
+            glTexCoord2f(1,1);
+            glVertex2f(shuriken.largura/2, shuriken.altura/2);
+
+            glTexCoord2f(0,1);
+            glVertex2f(-shuriken.largura/2, shuriken.altura/2);
+    }
+    else if(x==2){
+        glBindTexture(GL_TEXTURE_2D, id_textura_projetil_inimigo);
+        glPushMatrix();
+
+        glTranslatef(kunai.posX,kunai.posY,0);
+        
+        glBegin(GL_TRIANGLE_FAN);
+            glTexCoord2f(0,0);
+            glVertex2f(-kunai.largura/2, -kunai.altura/2);
+
+            glTexCoord2f(1,0);
+            glVertex2f(kunai.largura/2, -kunai.altura/2);
+            
+            glTexCoord2f(1,1);
+            glVertex2f(kunai.largura/2, kunai.altura/2);
+
+            glTexCoord2f(0,1);
+            glVertex2f(-kunai.largura/2, kunai.altura/2);
+    }
     glEnd();
     glPopMatrix();
     glDisable(GL_TEXTURE_2D);
@@ -145,11 +195,14 @@ bool colisao(int posX_projetil,
             int posX_inimigo, 
             int posY_inimigo, 
             int altura_inimigo, 
-            int largura_inimigo){
+            int largura_inimigo,
+            bool vivo){
 
     if(posY_projetil+altura_projetil == posY_inimigo){
         if(posX_projetil+largura_projetil/2 >= posX_inimigo && posX_projetil+largura_projetil/2 <= posX_inimigo+largura_inimigo){
-            return true;        
+            if(vivo){
+                return true;        
+            }        
         }    
     }
     
@@ -159,22 +212,23 @@ bool colisao(int posX_projetil,
 
 void desenhaMinhaCena(){
     glClear(GL_COLOR_BUFFER_BIT);
+    
     desenhaFundo();
     desenhaPersonagemPrincipal();
 
     criaMatrizInimigos();
     
     if(shuriken.atirar){
-        desenhaProjetil();
+        desenhaProjetil(1);
         for(int i=0;i<3;i++){
             for(int j=0;j<10;j++){
 
                 if(colisao(shuriken.posX,shuriken.posY, shuriken.largura,    
                            shuriken.altura, matriz_inimigos[i][j].posX,
                            matriz_inimigos[i][j].posY, matriz_inimigos[i][j].altura,
-                           matriz_inimigos[i][j].largura)){
+                           matriz_inimigos[i][j].largura,matriz_inimigos[i][j].vivo)){
 
-                           matriz_inimigos[i][j].vivo = false; 
+                           matriz_inimigos[i][j].vivo = false;                            
                            shuriken.atirar = false;         
                                                           }
             }
@@ -184,6 +238,10 @@ void desenhaMinhaCena(){
             shuriken.atirar = false;  
         }
     }
+    if(pause){ 
+        desenhaPause(); 
+    }
+    
     glutSwapBuffers();   
    
 }
@@ -209,7 +267,19 @@ void teclaPressionada(unsigned char tecla, int x, int y){
                 shuriken.atirar = true;
                 glutPostRedisplay();
             }
-            break;    
+            break; 
+        case 'p':
+        case 'P':
+            if(!pause){
+                pause = true;    
+                  
+                break;            
+            }else{
+                pause = false;
+                glutTimerFunc(1000/33,gameloop,1); 
+                break;
+            }
+            break;   
         default:
             break;        
     }
@@ -217,28 +287,29 @@ void teclaPressionada(unsigned char tecla, int x, int y){
 
 void teclaIPressionada(int tecla, int x, int y){
     switch(tecla){
-    case GLUT_KEY_RIGHT: // Seta para a direita ->
-        if(personagem_principal.posX < 90){
-            personagem_principal.posX++;        
-        }
+        case GLUT_KEY_RIGHT: // Seta para a direita ->
+            if(personagem_principal.posX < 90){
+                personagem_principal.posX++;        
+            }
+            
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_LEFT: // Seta para a esquerda <-
+            if(personagem_principal.posX > 10){
+                personagem_principal.posX--;        
+            }
+            glutPostRedisplay();
+            break;
         
-        glutPostRedisplay();
-        break;
-    case GLUT_KEY_LEFT: // Seta para a esquerda <-
-        if(personagem_principal.posX > 10){
-            personagem_principal.posX--;        
+        default:
+            break;
         }
-        glutPostRedisplay();
-        break;
-    default:
-        break;
-    }
 }
 
 void gameloop(int tempo){
 // DELIMITA ATÉ ONDE A TROPA VAI
-    if(flag != 0){
-        if(inimigo.posX <35){
+    if(flag){
+        if(inimigo.posX <34){
             inimigo.posX++;        
         }
         else{
@@ -247,7 +318,7 @@ void gameloop(int tempo){
         }
     }
     else{
-        if(inimigo.posX>0){
+        if(inimigo.posX>3){
             inimigo.posX--;
         }
         else{
@@ -256,9 +327,19 @@ void gameloop(int tempo){
         }
     }
 // "BORDA" DA TELA
-    glutPostRedisplay(); // PEDE PARA A CENA SER REDESENHADA ASSIM QUE POSSIVEL
-    glutTimerFunc(tempo, gameloop, tempo);// CHAMA NOVAMENTE A FUNÇÃO DE ATT A TELA (recursividade).
+    
+    if(!pause){      
+        glutPostRedisplay(); // PEDE PARA A CENA SER REDESENHADA ASSIM QUE POSSIVEL 
+        glutTimerFunc(1000/33,gameloop,1);// CHAMA NOVAMENTE A FUNÇÃO DE ATT A TELA (recursividade).
+                
+    }else{
+        desenhaPause();           
+        glutPostRedisplay(); 
+         
+    }
+  
 }
+
 
 
 void defineAtributos(){
@@ -271,10 +352,15 @@ void defineAtributos(){
     inimigo.posY = 80;
     inimigo.largura = 7;
     inimigo.altura = 7;
+    
+    kunai.largura = 4;
+    kunai.altura = 4;
+    kunai.atirar = false;
 
     shuriken.largura = 4;
     shuriken.altura = 4;
     shuriken.atirar = false;
+
     for(int i=0;i<3;i++){
         for(int j = 0;j<10;j++){
             matriz_inimigos[i][j].vivo = true;        
@@ -307,8 +393,9 @@ int main(int argc, char** argv){
     glutKeyboardFunc(teclaPressionada);
     glutSpecialFunc(teclaIPressionada);
     
-    glutTimerFunc(0,gameloop,132);   
-
+    glutTimerFunc(1000/33,gameloop,1);   
+    
+    
     setup();
     glutMainLoop();
     return 0;
