d26f08e2553161bb8f35951600e1b6c969cd8abb
minhoad <darmes2000@hotmail.com>
2021-02-28 20:44:06 -0300

    Adicionando struct de animação

--- global.h
+++ global.h
@@ -35,6 +35,24 @@ typedef struct{
     int qtdvidas; 
 }formacao;
 
+typedef struct{
+    double vert1_x;
+    double vert1_y;
+    double vert2_x;
+    double vert2_y;
+    double vert3_x;
+    double vert3_y;
+    double vert4_x;
+    double vert4_y;
+    double frameAtual;
+    double somador;
+    double qtdAtual;
+    double qtdTotalFrame;
+    double contadorDeFrame;
+    double velocidadeDoFrame;
+}animacao;
+
+
 int cont=0;
 
 // 3 shurikens; atirar mais rapido; vara ; em area*/
